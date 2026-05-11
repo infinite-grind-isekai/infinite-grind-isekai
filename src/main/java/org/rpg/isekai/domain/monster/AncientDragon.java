69a46ce472d08d6ba6391fd59da0ac4da563d135
dh0250 <hyun02549@gmail.com>
2026-05-11 21:32:42 +0900

    fix: 몬스터 생성자 버그 수정

--- src/main/java/org/rpg/isekai/domain/monster/AncientDragon.java
+++ src/main/java/org/rpg/isekai/domain/monster/AncientDragon.java
@@ -1,5 +1,6 @@
 package org.rpg.isekai.domain.monster;
 
+import org.rpg.isekai.domain.character.Stat;
 import org.rpg.isekai.domain.iface.Damageable;
 import org.rpg.isekai.domain.skill.Skill;
 import org.rpg.isekai.domain.skill.monster.FireBreath;
@@ -11,7 +12,7 @@ import java.util.List;
 public class AncientDragon extends Monster {
 
     public AncientDragon() {
-        super("고대 드래곤", 100, 5000, MonsterType.BOSS, new ArrayList<>(List.of(new FireBreath(), new DragonTail())));
+        super("고대 드래곤", 100, MonsterType.BOSS, new Stat(150, 80, 5000, 1000), new ArrayList<>(List.of(new FireBreath(), new DragonTail())));
     }
 
     @Override
--- src/main/java/org/rpg/isekai/domain/monster/Slime.java
+++ src/main/java/org/rpg/isekai/domain/monster/Slime.java
@@ -1,5 +1,6 @@
 package org.rpg.isekai.domain.monster;
 
+import org.rpg.isekai.domain.character.Stat;
 import org.rpg.isekai.domain.iface.Damageable;
 import org.rpg.isekai.domain.skill.Skill;
 import org.rpg.isekai.domain.skill.monster.SlimeBash;
@@ -11,7 +12,7 @@ import java.util.List;
 public class Slime extends Monster {
 
     public Slime() {
-        super("슬라임", 1, 50, MonsterType.NORMAL, new ArrayList<>(List.of(new SlimeBash(), new SlimeSticky())));
+        super("슬라임", 1, MonsterType.NORMAL, new Stat(5, 2, 50, 10), new ArrayList<>(List.of(new SlimeBash(), new SlimeSticky())));
     }
 
     @Override
