5f2ab126404eb9614271857bf44f04250d391c6a
yyubin <hazing120@gmail.com>
2026-05-12 16:31:08 +0900

    feat: add ItemManager, refactor GameController for item management, and clean unused InputHandler interface

---
+++ src/main/java/org/rpg/isekai/controller/ItemManager.java
@@ -0,0 +1,59 @@
+package org.rpg.isekai.controller;
+
+import lombok.Getter;
+import org.rpg.isekai.domain.item.Item;
+import org.rpg.isekai.domain.item.amorItem.*;
+import org.rpg.isekai.domain.item.materialItem.*;
+import org.rpg.isekai.domain.item.potionItem.*;
+import org.rpg.isekai.domain.item.weaponItem.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+public class ItemManager implements Manager{
+    private final List<Item> storeItems;
+
+    public ItemManager() {
+        storeItems = List.of(
+                // 포션류
+                new HealthPotion(),
+                new ManaPotion(),
+                new EnergyDrink(),
+                new HeartOfDragon(),
+                new PhoenixFeather(),
+                // 무기류
+                new IronSword(),
+                new MagicStaff(),
+                new DoomBringer(),
+                new ShadowBow(),
+                new ThunderstrikeGun(),
+                // 방어구류
+                new LeatherArmor(),
+                new IronPlate(),
+                new TitanArmor(),
+                new FrostguardShield(),
+                new DragonScaleMail(),
+                // 재료류
+                new SlimeJelly(),
+                new SkeletonBone(),
+                new GoblinEar(),
+                new OrcTooth(),
+                new DragonScale()
+        );
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean needPrepare() {
+        return true;
+    }
+
+    @Override
+    public void prepare() {
+    }
+}
